3083dacca8cb487079e06c169e62f99c9f616d1e
yafimvo <yafimvo@gmail.com>
2025-06-24 12:24:54 +0300

    v1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("TEST:: Uber pickups in NYC")
+st.title("TEST:: Uber pickups in NYC...")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
